325152e55de0ab03a855c3cc50dc9bbee38ff9a7
Irs444 <irs786had123@gmail.com>
2024-08-13 14:45:17 +0530

    home page

--- client/src/components/HomeScreen.jsx
+++ client/src/components/HomeScreen.jsx
@@ -1,45 +1,71 @@
-import { Button, Image, ImageBackground, ScrollView, StyleSheet, Text, View } from 'react-native'
+import { ImageBackground, ScrollView, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { Button, IconButton } from 'react-native-paper'
 
 const HomeScreen = ({ navigation }) => {
   return (
-    <View>
-      <ScrollView>
+    
+   
         <View style={styles.container}>
-          <ImageBackground source={require("../../assets/coffee.jpeg")} style={{ width: "100%", height:800 }}/>
-         
-          <View>
-            <Text>Best Mobile App Template for Your Bussiness</Text>
-            <Text>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat, optio?</Text>
-            <View>
-              <Button title='Explore Now' />
+          <ImageBackground source={require("../../assets/download.jpeg")} resizeMode='cover' style={styles.img}>
+
+            <View style={styles.text_container}>
+              <Text style={styles.text}>Best Mobile App Template for Your Bussiness</Text>
+              <Text style={styles.text1}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat, optio?</Text>
+              <View style={styles.btn}>
+                <Button onPress={() => navigation.navigate("Login")} mode='outlined' style={{color:"white"}} >Get Started</Button>
+              </View>
             </View>
-          </View>
+          </ImageBackground>
+
+
         </View>
-      </ScrollView>
-    </View>
+      
+   
   )
 }
 
 export default HomeScreen
 
 const styles = StyleSheet.create({
-container:{
-   flex:1,
+  container: {
+    flex: 1,
+     height:"100%"
+       
+  },
+  img:{
+    flex:1,
     justifyContent:"center",
+    opacity:0.9,
+   
+   
+  }, 
+  btn:{
+    marginHorizontal:20,
+    elevation:15,
+    marginBottom:20,
     
-    
-},
-hero:{
-  backgroundColor:"lightgrey",
-  padding:10,
-  borderRadius:10,
-  margin:10
-},
-heroImg:{
-  width:"100%",
-  height:400,
-  mixBlendMode:"multiply"
+  },
+  text_container:{
+   backgroundColor:"rgba(0,0,0,0.5)",
+   padding:10,
+   margin:10,
+   borderRadius:10
+  },
+  text:{
+    color:"white",
+    fontSize:40,
+    fontWight:"bold",
+    textAlign:"center",
+    marginBottom:10,
+    elevation:15
+  },
+  text1:{
+   color:"white",
+   textAlign:"center",
+   fontSize:20,
+   marginBottom:20 
+  }
  
-}
+ 
 })
