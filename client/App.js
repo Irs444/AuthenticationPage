2473de82942ed06c642a7457775b022f5b6ab1c8
Irs444 <irs786had123@gmail.com>
2024-08-15 00:10:49 +0530

    bottom bar completed

--- client/App.js
+++ client/App.js
@@ -7,25 +7,92 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import AddScreen from './src/components/AddScreen';
 import ProductScreen from './src/components/ProductScreen';
 import { UserProvider } from './src/context/userContext';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from "react-native-vector-icons/Ionicons"
+import FontAwesome from "react-native-vector-icons/FontAwesome"
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons"
 
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+
+  const Tab = createBottomTabNavigator()
+
+  const tabConfig =[
+    {
+      name:"Home",
+      component:HomeScreen,
+      focusedIcon:"home",
+      unfocusedIcon:"home-outline",
+      iconComponent:Ionicons
+      
+    },
+   {
+    name:"AddProduct",
+    component:AddScreen,
+    focusedIcon:"add-circle",
+    unfocusedIcon:"add",
+    iconComponent:Ionicons
+   },
+   {
+    name:'Product',
+    component:ProductScreen,
+    focusedIcon:"list-circle",
+    unfocusedIcon:"list",
+    iconComponent:Ionicons
+   },
+   {
+    name:"Signup",
+    component:SignupScreen,
+    focusedIcon:"user",
+    unfocusedIcon:"user-o",
+    iconComponent:FontAwesome
+   },
+   {
+    name:"Login",
+    component:LoginScreen,
+    focusedIcon:"login",
+    unfocusedIcon:"login-variant",
+    iconComponent:MaterialCommunityIcons
+   },
+  
+  ]
+
+  const screenOptions = ({route}) => ({
+    tabBarIcon: ({focused , color, size}) => {
+      const routeConfig = tabConfig.find(config => config.name === route.name)
+      const iconName = focused ? routeConfig.focusedIcon : routeConfig.unfocusedIcon;
+      const IconComponent = routeConfig.iconComponent;
+
+      return <IconComponent name={iconName}size={size} color={color}/>
+    },
+    tabBarActiveTintColor: "bue",
+    tabBarInactiveTintColor:"black",
+    tabBarLabelStyle:{
+      fontSize:10,
+      paddingBottom:5,
+      fontWeight:600,
+    },
+    tabBarStyle:{
+      height:60,
+      paddingTop:0
+    },
+  })
+
   return (
 
     <NavigationContainer>
       <UserProvider>
-        <Stack.Navigator initialRouteName='Home'>
-          <Stack.Screen name='Home' component={HomeScreen} />
-          <Stack.Screen name='Signup' component={SignupScreen} />
-          <Stack.Screen name='Login' component={LoginScreen} />
-          <Stack.Screen name='Loading' component={LoadingScreen} />
-          <Stack.Screen name='AddProduct' component={AddScreen} />
-          <Stack.Screen name='Product' component={ProductScreen} />
-
-        </Stack.Navigator>
-
+        <Tab.Navigator screenOptions={screenOptions}>
+          <Tab.Screen name='Home' component={HomeScreen} />
+          <Tab.Screen name='AddProduct' component={AddScreen} />
+          <Tab.Screen name='Product' component={ProductScreen} />
+          <Tab.Screen name='Signup' component={SignupScreen} />
+          <Tab.Screen name='Login' component={LoginScreen} />
+          {/* <Tab.Screen name='Loadingg' component={LoadingScreen} /> */}
+         
+        </Tab.Navigator>
       </UserProvider>
     </NavigationContainer>
 
